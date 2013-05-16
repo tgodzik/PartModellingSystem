b980175f363e9041b256e1752ab0feb94fad5cf4
Tomasz Godzik <tomek.godzik@gmail.com>
2013-05-16 14:28:36 +0200

    cylinder, sphere and cube on step slope

---
+++ object_simulation/ode_shapes/shapes.py
@@ -0,0 +1,60 @@
+__author__ = 'tomasz'
+
+import ode
+
+# world - ode world, space - ode space   
+def create_box(world, space, density, lx, ly, lz):
+
+    # Create body
+    body = ode.Body(world)
+    M = ode.Mass()
+    M.setBox(density, lx, ly, lz)
+    body.setMass(M)
+
+    # Set parameters for drawing the body
+    body.shape = "box"
+    body.box_size = (lx, ly, lz)
+
+    # Create a box geom for collision detection
+    geom = ode.GeomBox(space, lengths=body.box_size)
+    geom.setBody(body)
+
+    return body, geom
+
+# world - ode world, space - ode space
+def create_sphere(world, space, density, radius):
+
+    # Create body
+    body = ode.Body(world)
+    M = ode.Mass()
+    M.setSphere(density,radius)
+    body.setMass(M)
+
+    # Set parameters for drawing the body
+    body.shape = "sphere"
+    body.sphere_size = (radius)
+
+    # Create a box geom for collision detection
+    geom = ode.GeomSphere(space,radius)
+    geom.setBody(body)
+
+    return body, geom
+
+# world - ode world, space - ode space, axis - 1-x, 2-y, 3-z,
+def create_cylinder(world, space, density, radius, height,axis):
+
+    # Create body
+    body = ode.Body(world)
+    M = ode.Mass()
+    M.setCylinder(density,axis,radius,height)
+    body.setMass(M)
+
+    # Set parameters for drawing the body
+    body.shape = "cylinder"
+    body.cylinder_size = (radius,axis,height)
+
+    # Create a box geom for collision detection
+    geom = ode.GeomCylinder(space,radius,height)
+    geom.setBody(body)
+
+    return body, geom

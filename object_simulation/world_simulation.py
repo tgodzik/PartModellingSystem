5ffb7b57d846e6ccc0d8b0c483fdefb318fd83b4
Tomasz Godzik <tomek.godzik@gmail.com>
2013-06-06 00:51:28 +0200

    More detailed floor.

--- object_simulation/world_simulation.py
+++ object_simulation/world_simulation.py
@@ -4,7 +4,6 @@ from sim.simulation import Simulation
 
 
 def fight(object, other):
-
     fit1 = object.fitness()
     fit2 = other.fitness()
 
@@ -15,33 +14,48 @@ def fight(object, other):
         object.energy -= 100
         other.energy += 100
 
+
 def fitness(self):
-    return self.color[1]-self.color[0]-self.color[2]
+    return self.color[1] - self.color[0] - self.color[2]
 
-board_size=20
+board_size = 20
 VERTICES = [(board_size / 2, -1.0, board_size / 2),
-                 (0.0, -1.0, board_size / 2),
-                 (-board_size / 2, 1.0,board_size/ 2),
-                 (-board_size / 2, 1.0, 0.0),
-                 (-board_size / 2, 0.0, -board_size / 2),
-                 (0.0, 0.0, -board_size / 2),
-                 (board_size / 2, -1.0, -board_size / 2),
-                 (board_size / 2, 0.0, 0.0),
-                 (0.0, 1.0, 0.0)]
+            (0.0, -1.0, board_size / 2),
+            (-board_size / 2, 1.0, board_size / 2),
+            (-board_size / 2, 1.0, 0.0),
+            (-board_size / 2, 0.0, -board_size / 2),
+            (0.0, -1.0, -board_size / 2),
+            (board_size / 2, -1.0, -board_size / 2),
+            (board_size / 2, 0.0, 0.0),
+            (board_size / 4, 1.0, board_size / 4),
+            (-board_size / 4, 2.0, board_size / 4),
+            (-board_size / 4, 0.0, -board_size / 4),
+            (board_size / 4, -2.0, -board_size / 4),
+            (0.0, 2.0, 0.0)]
 
-INDICES = [(0, 8, 1), (1, 8, 2), (2, 8, 3), (3, 8, 4),
-                (4, 8, 5),
-                (5, 8, 6),
-                (6, 8, 7),
-                (7, 8, 0)]
+INDICES = [(0, 8, 1),
+           (1, 8, 12),
+           (1,9,2),
+           (1,12,9),
+           (2,9,3),
+           (9,12,3),
+           (3,12,10),
+           (3,10,4),
+           (4,10,5),
+           (12,5,10),
+           (6,5,11),
+           (11,5,12),
+           (6,11,7),
+           (7,11,12),
+           (0,7,8),
+           (8,7,12)]
 
 if __name__ == '__main__':
-
     parameters = {
         "func_fight": fight,
         "maximum_iterations": 1000,
-        "indices":INDICES,
-        "vertices":VERTICES
+        "indices": INDICES,
+        "vertices": VERTICES
     }
 
     sim = Simulation(20, board_size, parameters)
